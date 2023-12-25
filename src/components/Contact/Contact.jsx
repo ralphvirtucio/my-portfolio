53d1c24ba686ac26c41750abff94531beed0b92a
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-12-25 20:30:02 +0800

    Added rings and circle pattern base on figma design

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -34,6 +34,49 @@ export const Contact = () => {
           Send Message
         </PrimaryButton>
       </form>
+      <span className={styles.contact__ring}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='530'
+          height='129'>
+          <g
+            fill='none'
+            fillRule='evenodd'
+            stroke='#FFF'
+            opacity='.25'>
+            <ellipse
+              cx='265'
+              cy='40'
+              rx='264.5'
+              ry='39.5'
+            />
+            <ellipse
+              cx='265'
+              cy='52'
+              rx='264.5'
+              ry='39.5'
+            />
+            <ellipse
+              cx='265'
+              cy='65'
+              rx='264.5'
+              ry='39.5'
+            />
+            <ellipse
+              cx='265'
+              cy='77'
+              rx='264.5'
+              ry='39.5'
+            />
+            <ellipse
+              cx='265'
+              cy='89'
+              rx='264.5'
+              ry='39.5'
+            />
+          </g>
+        </svg>
+      </span>
     </section>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,6 +6,65 @@ export const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.hero__content}>
+        <span className={styles.hero__ring}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='530'
+            height='129'>
+            <g
+              fill='none'
+              fillRule='evenodd'
+              stroke='#FFF'
+              opacity='.25'>
+              <ellipse
+                cx='265'
+                cy='40'
+                rx='264.5'
+                ry='39.5'
+              />
+              <ellipse
+                cx='265'
+                cy='52'
+                rx='264.5'
+                ry='39.5'
+              />
+              <ellipse
+                cx='265'
+                cy='65'
+                rx='264.5'
+                ry='39.5'
+              />
+              <ellipse
+                cx='265'
+                cy='77'
+                rx='264.5'
+                ry='39.5'
+              />
+              <ellipse
+                cx='265'
+                cy='89'
+                rx='264.5'
+                ry='39.5'
+              />
+            </g>
+          </svg>
+        </span>
+
+        <span className={styles.hero__circle}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='129'
+            height='129'>
+            <circle
+              cx='830.5'
+              cy='585.5'
+              r='64'
+              fill='none'
+              stroke='#FFF'
+              transform='translate(-766 -521)'
+            />
+          </svg>
+        </span>
         <h2 className={styles['hero__content--heading']}>
           Nice to meet you! I&apos;m{' '}
           <span className={styles.hero__name}>Ralph Virtucio</span>.
