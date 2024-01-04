cdababe0c31ab043733593af4d8d395bb95c67cb
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-01-04 11:30:34 +0800

    fix: background ring and circle cut out

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,35 +7,37 @@ import styles from './Hero.module.css';
 export const Hero = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.hero__content}>
-        <h2 className={styles['hero__content--heading']}>
-          Nice to meet you! I&apos;m{' '}
-          <span className={styles.hero__name}>Ralph Virtucio</span>.
-        </h2>
-        <p className={styles['hero__content--text']}>
-          Based in the Philippines, I&apos;m a front-end developer passionate
-          about building accessible web apps that users love.
-        </p>
+      <div className={styles.container}>
+        <div className={styles.hero__content}>
+          <h2 className={styles['hero__content--heading']}>
+            Nice to meet you! I&apos;m{' '}
+            <span className={styles.hero__name}>Ralph Virtucio</span>.
+          </h2>
+          <p className={styles['hero__content--text']}>
+            Based in the Philippines, I&apos;m a front-end developer passionate
+            about building accessible web apps that users love.
+          </p>
 
-        <ButtonLink link='#'>Contact Me</ButtonLink>
-      </div>
+          <ButtonLink link='#'>Contact Me</ButtonLink>
+        </div>
 
-      <div className={styles.hero__img}>
-        <picture>
-          <source
-            media='(min-width: 48rem)'
-            srcSet={profileTablet}
-          />
-          <source
-            media='(min-width: 64rem)'
-            srcSet={profileDesktop}
-          />
+        <div className={styles.hero__img}>
+          <picture>
+            <source
+              media='(min-width: 48rem)'
+              srcSet={profileTablet}
+            />
+            <source
+              media='(min-width: 64rem)'
+              srcSet={profileDesktop}
+            />
 
-          <img
-            src={profileMobile}
-            alt='Profile'
-          />
-        </picture>
+            <img
+              src={profileMobile}
+              alt='Profile'
+            />
+          </picture>
+        </div>
       </div>
     </section>
   );
